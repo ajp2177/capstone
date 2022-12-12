9e5f80920ad8bcdb03756d1812ec01eda43f3034
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-12 12:00:43 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -96,23 +96,23 @@ df_to_predict = batter_2022_df.drop(columns = ['Name', '2022 Salary'])
 #predictions_2022 = bb_model.predict(df_to_predict)
 
 # Add prediction column
-batter_2022_df["Predicted Salary"] = np.around(np.exp(predictions_2022),0)
+#batter_2022_df["Predicted Salary"] = np.around(np.exp(predictions_2022),0)
 
 # Add value column
-batter_2022_df.loc[batter_2022_df['Predicted Salary'] > batter_2022_df['2022 Salary'], 'Value?'] = 'Under-valued'
-batter_2022_df.loc[batter_2022_df['Predicted Salary'] < batter_2022_df['2022 Salary'], 'Value?'] = 'Over-valued'
+#batter_2022_df.loc[batter_2022_df['Predicted Salary'] > batter_2022_df['2022 Salary'], 'Value?'] = 'Under-valued'
+#batter_2022_df.loc[batter_2022_df['Predicted Salary'] < batter_2022_df['2022 Salary'], 'Value?'] = 'Over-valued'
 
 # reorder columns
-batter_2022_df = batter_2022_df[['Name', '2022 Salary', 'Predicted Salary', 'Value?', 'Avg Career Salary Difference', 'Age', \
+#batter_2022_df = batter_2022_df[['Name', '2022 Salary', 'Predicted Salary', 'Value?', 'Avg Career Salary Difference', 'Age', \
                                 'H', 'R', 'RBI', 'BB', 'SO', 'SB', 'OPS']]
 
 # formatting as Millions
-batter_2022_df['2022 Salary'] = batter_2022_df['2022 Salary'].div(1000000).round(2)
-batter_2022_df['Predicted Salary'] = batter_2022_df['Predicted Salary'].div(1000000).round(2)
-batter_2022_df['Avg Career Salary Difference'] = batter_2022_df['Avg Career Salary Difference'].div(1000000).round(2)
+#batter_2022_df['2022 Salary'] = batter_2022_df['2022 Salary'].div(1000000).round(2)
+#batter_2022_df['Predicted Salary'] = batter_2022_df['Predicted Salary'].div(1000000).round(2)
+#batter_2022_df['Avg Career Salary Difference'] = batter_2022_df['Avg Career Salary Difference'].div(1000000).round(2)
 
-batter_2022_df = batter_2022_df.rename(columns = {'2022 Salary':'2022 Salary ($ Millions)',
+#batter_2022_df = batter_2022_df.rename(columns = {'2022 Salary':'2022 Salary ($ Millions)',
                                                   'Predicted Salary':'Predicted Salary ($ Millions)',
                                                   'Avg Career Salary Difference':'Avg Career Salary Difference ($ Millions)'})
 
-st.dataframe(batter_2022_df)
+#st.dataframe(batter_2022_df)
