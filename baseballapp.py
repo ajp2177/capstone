50ad235f9c1b8444b456b6f15e2b1a5f98e2e3a4
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-13 23:28:42 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -112,6 +112,23 @@ batter_2022_df['Avg Career Salary Difference'] = batter_2022_df['Avg Career Sala
 batter_2022_df = batter_2022_df.rename(columns = {'2022 Salary':'2022 Salary ($ Millions)',
                                                  'Predicted Salary':'Predicted Salary ($ Millions)',
                                                  'Avg Career Salary Difference':'Avg Career Salary Difference ($ Millions)'})
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+icon("search")
+selected = st.text_input("", "Search...")
+button_clicked = st.button("OK")
+
 
 st.dataframe(batter_2022_df)
 
