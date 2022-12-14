cc3173139c7a2a3ddb2107dbf036836f4f3011c3
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-14 00:33:26 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -16,6 +16,8 @@ if choice == "Home":
   st.markdown("<h1 style='text-align: center; color: green;'>The Value of MLB Players</h1>", unsafe_allow_html=True)
   
   st.image('https://www.sportico.com/wp-content/uploads/2022/04/Valuation_List_1280x720-1.png?w=1280&h=720&crop=1')
+  
+  st.markdown("<h1 style='text-align: center; color: black;'>Many players in the MLB are paid millions of dollars to underperform. This application provides a model to predict the salary that should be given to players based on performance metrics.</h1>", unsafe_allow_html=True)
 
 elif choice == "Predict Player Salary":
  
