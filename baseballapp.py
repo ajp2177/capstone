985428cb9f8eefc466ca12c33461e9124b41fc7e
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-14 00:09:29 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -123,7 +123,7 @@ st.table(batter_2022_df)
 
 selected_reward = st.selectbox("Choose a Player", batter_2022_df.Name, 0)
 
-selected_reward_price = batter_2022_df.loc[batter_2022_df.Name == selected_reward]["*"].iloc[0]
+selected_reward_price = batter_2022_df.loc[batter_2022_df.Name == selected_reward].iloc[0:13]
 
 st.write(f'Price: {selected_reward_price}')
                
