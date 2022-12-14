1c68e931e07d3e2a03acd3800662cfc450c763fd
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-14 11:42:12 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -50,149 +50,147 @@ if check_password():
       message['Subject'] = Header(subject)
       message.attach(MIMEText(email_message,'plain', 'utf-8'))
   
+    st.sidebar.info("Welcome, please use dropdown box to navigate to other pages.")
 
-  st.sidebar.info("Welcome, please use dropdown box to navigate to other pages.")
-
-  pages = ["Home", "Plots", "Predict Player Salary"]
-  message = '''Select one of the options in the dropdown list to access specific page'''
-  choice = st.sidebar.selectbox("Choose a page: ",pages, help = message)
-
-
-  if choice == "Home":
-    st.markdown("<h1 style='text-align: center; color: green;'>The Value of MLB Players</h1>", unsafe_allow_html=True)
-
-    st.image('https://www.sportico.com/wp-content/uploads/2022/04/Valuation_List_1280x720-1.png?w=1280&h=720&crop=1')
-
-    st.markdown("This application provides a model to predict the salary that should be given to players based on performance metrics.", unsafe_allow_html=True)
-
-  elif choice == "Predict Player Salary":
-
-    st.markdown("<h1 style='text-align: center; color: green;'>Predict Player Salary</h1>", unsafe_allow_html=True)
-    st.image("capstonehome.jpeg", use_column_width= 'always')
-
-    st.markdown("Enter the following statistics for a batter and \
-                        get an estimated salary value.")
+      pages = ["Home", "Plots", "Predict Player Salary"]
+      message = '''Select one of the options in the dropdown list to access specific page'''
+      choice = st.sidebar.selectbox("Choose a page: ",pages, help = message)
 
 
-    difference = st.number_input("Average Salary Difference (in $)")
-    st.caption("*Note: Average Salary Difference is the average increase/decrease of a salary across a player's entire career. \
-    For example, Derek Jeter's Average Salary Difference between 2012 and 2013 would be \\$1M if his salary was \\$14M in 2012 and \\$15M in 2013")
+      if choice == "Home":
+        st.markdown("<h1 style='text-align: center; color: green;'>The Value of MLB Players</h1>", unsafe_allow_html=True)
+
+        st.image('https://www.sportico.com/wp-content/uploads/2022/04/Valuation_List_1280x720-1.png?w=1280&h=720&crop=1')
+
+        st.markdown("This application provides a model to predict the salary that should be given to players based on performance metrics.", unsafe_allow_html=True)
+
+      elif choice == "Predict Player Salary":
+
+        st.markdown("<h1 style='text-align: center; color: green;'>Predict Player Salary</h1>", unsafe_allow_html=True)
+        st.image("capstonehome.jpeg", use_column_width= 'always')
+
+        st.markdown("Enter the following statistics for a batter and \
+                            get an estimated salary value.")
 
 
-    age = st.slider('Age', 18, 45, 25)
+        difference = st.number_input("Average Salary Difference (in $)")
+        st.caption("*Note: Average Salary Difference is the average increase/decrease of a salary across a player's entire career. \
+        For example, Derek Jeter's Average Salary Difference between 2012 and 2013 would be \\$1M if his salary was \\$14M in 2012 and \\$15M in 2013")
 
 
-    hits = st.slider('Hits', 0, 250, 100)
+        age = st.slider('Age', 18, 45, 25)
 
 
-    runs= st.slider('Runs', 0, 200, 50)
+        hits = st.slider('Hits', 0, 250, 100)
 
 
-    rbi = st.slider('RBIs', 0, 200, 75)
+        runs= st.slider('Runs', 0, 200, 50)
 
 
-    walks = st.slider('Walks', 0, 250, 50)
+        rbi = st.slider('RBIs', 0, 200, 75)
 
 
-    so = st.slider('Strikeouts', 0, 250, 50)
+        walks = st.slider('Walks', 0, 250, 50)
 
 
-    sb = st.slider('Stolen Bases', 0, 100, 10)
+        so = st.slider('Strikeouts', 0, 250, 50)
 
 
-    ops = st.number_input("Enter OPS")
+        sb = st.slider('Stolen Bases', 0, 100, 10)
 
-    if st.button("Submit"):
 
-        # unpickle the batting model
+        ops = st.number_input("Enter OPS")
+
+        if st.button("Submit"):
+
+            # unpickle the batting model
+            bb_model = joblib.load("batting_basic_model.pkl")
+
+            # store inputs into df
+
+            column_names = ['Salary Difference', 'Age', 'H', 'R', 'RBI', 'BB', 'SO', 'SB', 'OPS']
+            df = pd.DataFrame([[difference, age, hits, runs, rbi, walks, so, sb, ops]], 
+                             columns = column_names)
+
+              # get prediction
+            prediction = bb_model.predict(df)
+            st.write(prediction)
+
+            # convert prediction
+            #converted = round(np.exp(prediction)[0],0)
+            #converted = np.exp(prediction)
+
+            with st.spinner('Calculating...'):
+                time.sleep(1)
+            st.success('Done!')
+
+            st.dataframe(df)
+
+            # output prediction
+            #st.header(f"Predicted Player Salary: ${converted:,}")
+            #st.header("Predicted Player Salary:", prediction)
+
+
+
+
+
+        # 2022 batter dataframe
+        batter_2022_df = pd.read_csv('batting_merged_2022', index_col = 0)
+        # reformat 2022 batter df for model prediction
+        df_to_predict = batter_2022_df.drop(columns = ['Name', '2022 Salary'])
+
+        # load in model
         bb_model = joblib.load("batting_basic_model.pkl")
 
-        # store inputs into df
+        # make prediction
+        predictions_2022 = bb_model.predict(df_to_predict)
 
-        column_names = ['Salary Difference', 'Age', 'H', 'R', 'RBI', 'BB', 'SO', 'SB', 'OPS']
-        df = pd.DataFrame([[difference, age, hits, runs, rbi, walks, so, sb, ops]], 
-                         columns = column_names)
+        # Add prediction column
+        batter_2022_df["Predicted Salary"] = np.around(np.exp(predictions_2022),0)
 
-          # get prediction
-        prediction = bb_model.predict(df)
-        st.write(prediction)
+        pred_button = st.button("Prediction comparisions")
 
-        # convert prediction
-        #converted = round(np.exp(prediction)[0],0)
-        #converted = np.exp(prediction)
+        if pred_button:
+          st.markdown("### Predictions vs. 2022 MLB Stats")
 
-        with st.spinner('Calculating...'):
-            time.sleep(1)
-        st.success('Done!')
+          # Add value column
+          batter_2022_df.loc[batter_2022_df['Predicted Salary'] > batter_2022_df['2022 Salary'], 'Value?'] = 'Under-valued'
+          batter_2022_df.loc[batter_2022_df['Predicted Salary'] < batter_2022_df['2022 Salary'], 'Value?'] = 'Over-valued'
 
-        st.dataframe(df)
+          # reorder columns
+          batter_2022_df = batter_2022_df[['Name', '2022 Salary', 'Predicted Salary', 'Value?', 'Avg Career Salary Difference', 'Age', \
+                                         'H', 'R', 'RBI', 'BB', 'SO', 'SB', 'OPS']]
 
-        # output prediction
-        #st.header(f"Predicted Player Salary: ${converted:,}")
-        #st.header("Predicted Player Salary:", prediction)
+          # formatting as Millions
+          batter_2022_df['2022 Salary'] = batter_2022_df['2022 Salary']
+          batter_2022_df['Predicted Salary'] = batter_2022_df['Predicted Salary']
+          batter_2022_df['Avg Career Salary Difference'] = batter_2022_df['Avg Career Salary Difference']
 
-
-
-
-
-    # 2022 batter dataframe
-    batter_2022_df = pd.read_csv('batting_merged_2022', index_col = 0)
-    # reformat 2022 batter df for model prediction
-    df_to_predict = batter_2022_df.drop(columns = ['Name', '2022 Salary'])
-
-    # load in model
-    bb_model = joblib.load("batting_basic_model.pkl")
-
-    # make prediction
-    predictions_2022 = bb_model.predict(df_to_predict)
-
-    # Add prediction column
-    batter_2022_df["Predicted Salary"] = np.around(np.exp(predictions_2022),0)
-
-    pred_button = st.button("Prediction comparisions")
-
-    if pred_button:
-      st.markdown("### Predictions vs. 2022 MLB Stats")
-
-      # Add value column
-      batter_2022_df.loc[batter_2022_df['Predicted Salary'] > batter_2022_df['2022 Salary'], 'Value?'] = 'Under-valued'
-      batter_2022_df.loc[batter_2022_df['Predicted Salary'] < batter_2022_df['2022 Salary'], 'Value?'] = 'Over-valued'
-
-      # reorder columns
-      batter_2022_df = batter_2022_df[['Name', '2022 Salary', 'Predicted Salary', 'Value?', 'Avg Career Salary Difference', 'Age', \
-                                     'H', 'R', 'RBI', 'BB', 'SO', 'SB', 'OPS']]
-
-      # formatting as Millions
-      batter_2022_df['2022 Salary'] = batter_2022_df['2022 Salary']
-      batter_2022_df['Predicted Salary'] = batter_2022_df['Predicted Salary']
-      batter_2022_df['Avg Career Salary Difference'] = batter_2022_df['Avg Career Salary Difference']
-
-      batter_2022_df = batter_2022_df.rename(columns = {'2022 Salary':'2022 Salary ($ Millions)',
-                                                       'Predicted Salary':'Predicted Salary ($ Millions)',
-                                                       'Avg Career Salary Difference':'Avg Career Salary Difference ($ Millions)'})
+          batter_2022_df = batter_2022_df.rename(columns = {'2022 Salary':'2022 Salary ($ Millions)',
+                                                           'Predicted Salary':'Predicted Salary ($ Millions)',
+                                                           'Avg Career Salary Difference':'Avg Career Salary Difference ($ Millions)'})
 
 
 
-      selected_reward = st.selectbox("Choose a Player", batter_2022_df.Name, 0)
+          selected_reward = st.selectbox("Choose a Player", batter_2022_df.Name, 0)
 
-      selected_reward_price = batter_2022_df.loc[batter_2022_df.Name == selected_reward].iloc[0:13]
+          selected_reward_price = batter_2022_df.loc[batter_2022_df.Name == selected_reward].iloc[0:13]
 
-      st.dataframe(selected_reward_price)
+          st.dataframe(selected_reward_price)
 
-      def convert_df(churn):
-        return churn.to_csv(index=False).encode('utf-8')
+          def convert_df(churn):
+            return churn.to_csv(index=False).encode('utf-8')
 
 
-        csv = convert_df(pd.DataFrame(selected_reward_price))
+            csv = convert_df(pd.DataFrame(selected_reward_price))
 
-        st.download_button(
-            "Download predictions",
-            csv,
-            "player_salary.csv",
-            "text/csv",
-            key='download-csv'
-        )
-                            
+            st.download_button(
+                "Download predictions",
+                csv,
+                "player_salary.csv",
+                "text/csv",
+                key='download-csv'
+            )
 
 
 
@@ -202,4 +200,5 @@ if check_password():
 
 
 
-    
+
+
