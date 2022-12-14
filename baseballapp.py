eb2c9bffe2edac5b0b164dd5255dc19b5c521453
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-14 00:39:28 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -32,36 +32,35 @@ elif choice == "Predict Player Salary":
   #st.sidebar.image("http://cdn.shopify.com/s/files/1/0480/9470/7866/collections/ef26964ae31041325cd9672682c01534.jpg?v=1646869133", width = 200)
   st.sidebar.markdown("How does a team determine how much to pay their players?")
 
-  # input bar 1
+  
   difference = st.number_input("Average Salary Difference (in $)")
   st.caption("*Note: Average Salary Difference is the average increase/decrease of a salary across a player's entire career. \
   For example, Derek Jeter's Average Salary Difference between 2012 and 2013 would be \\$1M if his salary was \\$14M in 2012 and \\$15M in 2013")
 
-  # input bar 2
+ 
   age = st.slider('Age', 18, 45, 25)
 
-  # input bar 3
+  
   hits = st.slider('Hits', 0, 250, 100)
 
-  # input bar 4
+  
   runs= st.slider('Runs', 0, 200, 50)
 
-  # input bar 5
+  
   rbi = st.slider('RBIs', 0, 200, 75)
 
-  # input bar 6
+  
   walks = st.slider('Walks', 0, 250, 50)
 
-  # input bar 7
+  
   so = st.slider('Strikeouts', 0, 250, 50)
 
-  # input bar 8
+  
   sb = st.slider('Stolen Bases', 0, 100, 10)
 
-  # input bar 9
+  
   ops = st.number_input("Enter OPS")
 
-  # if button is pressed
   if st.button("Submit"):
 
       # unpickle the batting model
