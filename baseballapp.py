f12dcb1db54ae49a693f8a94edc4a3d86877f6ca
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-14 23:16:42 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -90,9 +90,9 @@ if check_password():
             st.image("Screen Shot 2022-12-14 at 11.01.57 PM.png")
             
         elif sc == 'Boxplots':
-            st.image("")
+            st.image('
         
         elif sc == 'Position Group Totals':
-            st.image("")
+            st.image("Screen Shot 2022-12-14 at 11.10.29 PM.png")
            
     
