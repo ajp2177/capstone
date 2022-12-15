0465da8f13f8eada49671e29852c32b11c893577
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-14 22:47:31 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -192,8 +192,7 @@ if check_password():
       batter_2022_df = batter_2022_df.rename(columns = {'2022 Salary':'2022 Salary ($ Millions)',
                                                        'Predicted Salary':'Predicted Salary ($ Millions)',
                                                        'Avg Career Salary Difference':'Avg Career Salary Difference ($ Millions)'})
-
-      st.dataframe(batter_2022)
+st.dataframe(batter_2022_df)
 
       selected_reward = st.selectbox("Choose a Player", batter_2022_df.Name, 0)
 
