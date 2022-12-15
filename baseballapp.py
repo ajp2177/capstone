4427e15853993e7f0bab654d0a10a8d24585b14b
Andrew P <118642994+ajp2177@users.noreply.github.com>
2022-12-14 23:03:14 -0700

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -86,6 +86,8 @@ if check_password():
                                                                      'Boxplots',
                                                                      "Position Group Totals",
                                                                      ))
+        if sc == 'Histogram':
+            st.image("Screen Shot 2022-12-14 at 11.01.57 PM.png"
         
        
     
