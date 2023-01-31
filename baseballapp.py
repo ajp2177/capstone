b65beecf359681721212299243060eceb4315060
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-01-31 10:45:57 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -32,22 +32,22 @@ def check_password():
         )
         st.error("Incorrect password, please try again.")
         def main():
-    st.title("Send password to email")
-    activities =["Send Email"]
-    choice = st.sidebar.selectbox("Reset Password", activites)
-    if choice == "Send Email":
-        sender = "ajflash21@gmail.com"
-        password = "Offutlake2017"
-        reciever = st.text_input("Enter your email address")
-        if st.buttion("Send Email"):
-            try:
-                connection =s.SMTP('smtp.gmail.com', 587)
-                connection.starttls()
-                connection.login(sender,password)
-                connection.sendmail(sender, reciever)
-                connection.quit()
-if __name__=="__main__":
-    main()
+            st.title("Send password to email")
+            activities =["Send Email"]
+            choice = st.sidebar.selectbox("Reset Password", activites)
+            if choice == "Send Email":
+                sender = "ajflash21@gmail.com"
+                password = "Offutlake2017"
+                reciever = st.text_input("Enter your email address")
+                if st.buttion("Send Email"):
+                    try:
+                        connection =s.SMTP('smtp.gmail.com', 587)
+                        connection.starttls()
+                        connection.login(sender,password)
+                        connection.sendmail(sender, reciever)
+                        connection.quit()
+        if __name__=="__main__":
+            main()
         return False
     else:
         # Password correct.
