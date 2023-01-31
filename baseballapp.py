afa30d2ecfa4d4ad5326a16b19bf6d076bfafbd4
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-01-31 10:23:23 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -4,6 +4,23 @@ import pandas as pd
 import streamlit as st
 import joblib
 import time
+import smtplib as s
+
+def main():
+    st.title("Send password to email")
+    activities =["Send Email"]
+    choice = st.sidebar.selectbox("Reset Password", activites)
+    if choice == "Send Email":
+        sender = "ajflash21@gmail.com"
+        password = "Offutlake2017"
+        reciever = st.text_input("Enter your email address")
+        if st.buttion("Send Email"):
+            try:
+                connection =s.SMTP('smtp.gmail.com', 587)
+                connection.starttls()
+                connection.login(sender,password)
+                connection.sendmail(sender, reciever)
+                connection.quit()
 
 
 def check_password():
