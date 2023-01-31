abcf9d6f90b3942a31199b5c90b85e1084d84978
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-01-31 10:32:53 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -6,23 +6,6 @@ import joblib
 import time
 import smtplib as s
 
-def main():
-    st.title("Send password to email")
-    activities =["Send Email"]
-    choice = st.sidebar.selectbox("Reset Password", activites)
-    if choice == "Send Email":
-        sender = "ajflash21@gmail.com"
-        password = "Offutlake2017"
-        reciever = st.text_input("Enter your email address")
-        if st.buttion("Send Email"):
-            try:
-                connection =s.SMTP('smtp.gmail.com', 587)
-                connection.starttls()
-                connection.login(sender,password)
-                connection.sendmail(sender, reciever)
-                connection.quit()
-if __name__=="__main__":
-    main()
     
 def check_password():
     """Returns `True` if the user had the correct password."""
