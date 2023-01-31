6da4eb1afb16a6d0e3f858bd5a1675df3500689b
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-01-31 11:00:58 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -14,20 +14,20 @@ ebutton = st.button("Send email")
 
 def send_email(email, username, password_reset_link):
     # Build the email message
-        st.title("Send password to email")
-            activities =["Send Email"]
-            choice = st.sidebar.selectbox("Reset Password", activites)
-            if choice == "Send Email":
-                sender = "ajflash21@gmail.com"
-                password = "Offutlake2017"
-                reciever = st.text_input("Enter your email address")
-                if st.buttion("Send Email"):
-                    try:
-                        connection =s.SMTP('smtp.gmail.com', 587)
-                        connection.starttls()
-                        connection.login(sender,password)
-                        connection.sendmail(sender, reciever)
-                        connection.quit()
+    st.title("Send password to email")
+    activities =["Send Email"]
+    choice = st.sidebar.selectbox("Reset Password", activites)
+    if choice == "Send Email":
+        sender = "ajflash21@gmail.com"
+        password = "Offutlake2017"
+        reciever = st.text_input("Enter your email address")
+        if st.buttion("Send Email"):
+            try:
+                connection =s.SMTP('smtp.gmail.com', 587)
+                connection.starttls()
+                connection.login(sender,password)
+                connection.sendmail(sender, reciever)
+                connection.quit()
         
 
     
