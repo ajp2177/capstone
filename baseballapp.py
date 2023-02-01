98f019213a1a55b6705af07e431aebb144c1a41a
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-02-01 15:59:57 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -95,7 +95,7 @@ if check_password():
                     key='download-csv'
                 )
                 
-                st.markdown('The average MLB salary is  $5,553,397.74')
+                st.markdown('**The average MLB salary is  $5,553,397.74**')
                 
                 code = '''import statistics
 meansal = statistics.mean(df2.salary)
