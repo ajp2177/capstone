1746b448493fb1dff4b20a84d3aa3caccbf9e103
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-02-01 15:14:54 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -58,6 +58,7 @@ if check_password():
 
     elif choice == "Data Exploration":
         tab1, tab2 = st.tabs(["Exploratory Analysis", "Data Visualizations"])
+        learn =  st.sidebar.selectbox("What would you like assistance with?", listo)
         if listo == 'Page Description':
             st.sidebar.markdown('This is the Data Exploration page of the application to view some visualizations regarding the MLB data.') 
         with tab1:
@@ -98,7 +99,7 @@ if check_password():
         st.markdown("<h1 style='text-align: left; color: green;'>Predict MLB Player Salaries</h1>", unsafe_allow_html=True)
 
         st.image("https://cdn.vox-cdn.com/thumbor/BROKHvXvRY7VPqC2opTqGjKIksI=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13688709/TeamsHoardingCash_Getty_Ringer.jpg")
-
+        learn =  st.sidebar.selectbox("What would you like assistance with?", listo)
         if listo == 'Page Description':
             st.sidebar.markdown('This is the Predict Player page to generate player salaries based on the selected input.') 
         tab3, tab4 = st.tabs(["Hitters", "Pitchers"])
