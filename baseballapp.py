e2356a1a2a09cfbb5a5760fa09e8ddb8206e85c7
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-02-01 19:26:19 -0800

    Update baseballapp.py

--- baseballapp.py
+++ baseballapp.py
@@ -6,26 +6,34 @@ import joblib
 import time
 
 def check_password():
-    """True` for correct password."""
+    """Returns `True` if the user had the correct password."""
 
-    if "password_correct" not in st.session_state:
-        st.subheader("**Enter password to access application**")
-        password = st.text_input("Password:", type="password", key="password")
-        if password == st.secrets["password"]:
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
             st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
         else:
             st.session_state["password_correct"] = False
-            st.error("Invalid password, please try again.")
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.subheader("**Enter password to access application**")
+        st.text_input(
+            "Password:", type="password", on_change=password_entered, key="password"
+        )
         return False
     elif not st.session_state["password_correct"]:
-        password = st.text_input("Password:", type="password", key="password")
-        if password == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-        else:
-            st.error("Invalid password, please try again.")
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("Incorrect password, please try again.")
         return False
     else:
+        # Password correct.
         return True
+
 
 
 if check_password():
